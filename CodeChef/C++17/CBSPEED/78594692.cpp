7ac3e855718e6277d14b3f23eea7537f1e1f8cc2
Vishal <116947827+vishalkodes@users.noreply.github.com>
2022-11-06 18:02:34 +0530

    Committing files

---
+++ CodeChef/C++17/CBSPEED/78594692.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int c,t;
+        cin>>t>>c;
+        
+        if(c>t)
+            {printf("YES\n");}
+        else
+            {printf("NO\n");}
+	return 0;
+}
